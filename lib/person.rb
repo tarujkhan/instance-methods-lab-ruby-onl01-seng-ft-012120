5f30de4eb1a3d5167696170afaace4b769d77bbb
taroj khan <tarujkhan@yahoo.com>
2020-02-03 22:47:38 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person
+
+  def Person 
+    # Instance variables  
+   # @talk = talk 
+    #@walk = walk  
+  end
+  
+  def walk  
+    puts "The Person is walking" 
+  end  
+  
+  def talk  
+    puts "Hello World!"  
+  end  
